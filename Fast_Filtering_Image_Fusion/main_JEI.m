44ab3a0dcd81412b4ce878a49440465b24957003
SudheerReddyBandi <57788637+SudheerReddyBandi@users.noreply.github.com>
2023-04-10 14:29:55 +0530

    Add files via upload

---
+++ Fast_Filtering_Image_Fusion/main_JEI.m
@@ -0,0 +1,11 @@
+% addpath('./tools')
+I1 = imread('disk1.gif');
+I2 = imread('disk2.gif');
+I(:,:,1) = I1;
+I(:,:,2) = I2;
+tic
+
+% for the visible and infrared image r =16; lambda= 0.06
+F_ffif = FFIF(double(I),6,0.06);
+time_ffif = toc;
+% main_measure_fusion(I1,I2,uint8(F_ffif))
